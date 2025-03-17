faf03e40d8f2baf48376f3b53636415370ce49aa
Guilherme0112 <guimendesmen124@gmail.com>
2025-03-17 15:33:28 +0000

    Primeiros ajustes na tela de carrinho

--- src/main/resources/static/js/carrinho.js
+++ src/main/resources/static/js/carrinho.js
@@ -1,3 +1,32 @@
+function createBox(img, nome, quantidadeP, preco){
+
+    const divPai = document.createElement("div");
+    divPai.className = "box_carrinho";
+
+    const imgP = document.createElement("img");
+    imgP.className = "box_img_carrinho";
+    imgP.src = img;
+
+    const nomeProduto = document.createElement("p");
+    nomeProduto.className = "box_nome_produto";
+    nomeProduto.textContent = nome;
+
+    const price = document.createElement("p");
+    price.className = "box_preco_produto";
+    price.textContent = preco.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
+
+    const quantidade = document.createElement("p");
+    quantidade.className = "box_quantidade_produto";
+    quantidade.textContent = quantidadeP;
+
+    divPai.appendChild(imgP);
+    divPai.appendChild(nomeProduto);
+    divPai.appendChild(price);
+    divPai.appendChild(quantidade);
+
+    return divPai;
+}
+
 
 document.addEventListener("DOMContentLoaded", async() =>{
     var produtos = [];
@@ -20,5 +49,10 @@ document.addEventListener("DOMContentLoaded", async() =>{
     
     const data = await res.json();
     
-    console.log(data);
+    const carrinho = document.getElementById("carrinho");
+    let box = null;
+    data.forEach(dado => {
+        box = createBox(dado.photo, dado.name, getItemCarrinho(dado.codigo).quantidade, dado.price);
+        carrinho.appendChild(box);
+    });
 })
